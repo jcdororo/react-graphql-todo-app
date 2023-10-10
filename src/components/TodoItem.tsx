96e8c8b90b56100acd0c1c9b9e23c2ed332002c6
jc <wnwlcks123@gmail.com>
2023-10-10 21:55:26 +0900

    TodoItem 컴포넌트 생성

--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { FC, useState } from 'react'
+import { IList } from '../types';
+import { FiEdit, FiMinusCircle } from 'react-icons/fi'
 
-const TodoItem = ({item}) => {
+interface TodoItemProps {
+  item: IList;
+}
+
+const TodoItem: FC<TodoItemProps> = ({item}) => {
+  const [edit, setEdit] = useState(false);
+  const [task, setTask] = useState(item.text);
+
   return (
-    <div>TodoItem</div>
+    <li className='flex items-center justify-between p-5 my-3 text-2xl duration-300 hover:scale-105 border-2 rounded-md shadow-md'>
+      <div className='flex items-center w-10/12'>
+        <input checked={item.checked} type={'checkbox'} className='hover:scale-105 hover:cursor-pointer' />
+        <input 
+          type='text'
+          disabled={!edit}
+          value={task}
+          onChange={e => setTask(e.target.value)}
+          className={`outline-none h-[25px] text-xl w-full mx-5 px-3 disabled:bg-transparent focus:border-b-[1px] ${item.checked && 'line-through'} text-stone-500`}
+        />
+      </div>
+      <div className='flex justify-between w-1/6'>
+        <FiEdit className='hover:scale-105 hover:cursor-pointer' />
+        <FiMinusCircle className='hover:scale-105 hover:cursor-pointer' />
+      </div>
+
+    </li>
   )
 }
 
